2076eba1e514401f04cc2d696ca2904a38151923
alvaaroofs <afernandezs@uxcale.com>
2024-07-29 09:43:31 +0200

    Apartado 8 rutas

--- 8.routerdom/src/App.js
+++ 8.routerdom/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
+//==========================================
+//8. Este apartado, es muy necesario en webs donde haya muchisimas paginas dentro de ella; 
+//Instalaremos en una nueva terminal, una libreria para ello: npm install react-router-dom
+/*
+Router:
+Routes:
+Route: lo usaremos para definir cada una de las rutas que vamos a tener en la web
+*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
-  );
 }
 
 export default App;
