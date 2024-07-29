c24bcf7d9f03f32daf5fa751396f8dae7d0edc97
alvaaroofs <afernandezs@uxcale.com>
2024-07-29 12:57:19 +0200

    Navbar actualizado

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,10 @@ export const Navbar = () => {
   return (
     <div>
       <Link to="/"> Home </Link>
-      <Link to="/menu"> Menu </Link>
+      <Link to="/profile"> Profile </Link>
       <Link to="/contact"> Contact </Link>
     </div>
   );
 };
+
+//Importante sustituir lo de menu por profile, ya que si mantenemos la ruta de menu, nos dara error
