637f0985b97ce727ba116da765ddb8159bd6ed6e
alvaaroofs <afernandezs@uxcale.com>
2024-07-29 11:12:39 +0200

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import { useState } from "react";
 /*
 En este apartado 9, haremos uso del useContext; hemos sustituido la opcion de menu, por profile, y hemos creado la
 carpeta components, con un js de ChangeProfile en el que dentro de el explicaremos su funcionamiento.
+
+**El motivo por el que se produce la funcion de cambiar username que hacemos desde otros componentes, es porque ChangeProfile.js esta conectado con Profile.js (lo llamamos con <ChangeProfile /> y
+este esta conectado con App.js (actual file). Para que se cumpla la funcion, simplemente en <Route path="/profile" element={<Profile username={username} />} /> vamos
+a añadir a la derecha del username={username}, setUsername={setUsername}
 */
 function App() {
   const [username, setUsername] = useState("Alvaro F");
@@ -19,7 +23,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home username={username}/>} />
-          <Route path="/menu" element={<Profile username={username} />} />
+          <Route path="/profile" element={<Profile username={username} setUsername={setUsername}/>} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
         </Routes>
--- src/components/ChangeProfile.js
+++ src/components/ChangeProfile.js
@@ -1,3 +1,19 @@
-export const ChangeProfile = () => {
+import { useState } from "react";
 
+export const ChangeProfile = (props) => {
+    const [newUsername, setNewUsername] = useState("");
+    return(
+        <div>
+            <input onChange={(event) => {
+                setNewUsername(event.target.value);
+            }}/>
+            <button onClick={() => {
+                    props.setNewUsername(newUsername);
+                }}> 
+                Change the username 
+            </button>
+        </div>
+    );
 };
+
+//Vamos a crear un boton que, cuando lo clicemos, cambiaremos el valor de username (cambiaremos el nombre).
